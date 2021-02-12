08ecc5d1121ecdd5b297fdd25fb5b0da67e67956
gmonteiro13 <gscmonteiro@gmail.com>
2021-02-12 12:29:18 -0300

    Tested ft_substr

---
+++ ft_substr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel-scm <gabriel-scm@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/12 11:57:31 by gabriel-scm       #+#    #+#             */
+/*   Updated: 2021/02/12 12:27:08 by gabriel-scm      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	char			*substr;
+	unsigned int	i;
+
+	i = 0;
+	substr = malloc(len + 1);
+	if (substr == NULL || s == NULL)
+		return (NULL);
+	if (start < ft_strlen(s))
+	{
+		while (i < len && s[start + i] != '\0')
+		{
+			substr[i] = s[start + i];
+			i++;
+		}
+	}
+	substr[i] = '\0';
+	return (substr);
+}
